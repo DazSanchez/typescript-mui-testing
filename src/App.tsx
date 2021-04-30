df22ca92c20d3f79b55c0f589cadcc627b6d5bbb
Hugo Sánchez V <programacionhsv@gmail.com>
2021-04-30 02:39:01 -0500

    Agregado router con react-router
    - Se configura el proyecto para utilizar react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { addPrefetchExcludes, Root, Routes } from 'react-static';
+import { Route, Switch } from 'react-router-dom';
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes([]);
+
+const Lorem = () => <p>Lorem</p>;
 
 function App() {
   return (
     <Root>
       <React.Suspense fallback={'Loading...'}>
-        <Routes />
+        <Switch>
+          <Route path="/lorem" component={Lorem} />
+          <Route render={() => <Routes />} />
+        </Switch>
       </React.Suspense>
     </Root>
   );
--- static.config.js
+++ static.config.js
@@ -10,6 +10,7 @@ export default {
         location: path.resolve('./src/pages'),
       },
     ],
+    require.resolve('react-static-plugin-react-router'),
     require.resolve('react-static-plugin-sitemap'),
   ],
 };
